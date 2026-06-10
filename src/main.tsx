e411e70089557f1b4ccf6f970b2e7d7a3858523a
naya <nastyasytnik6@gmail.com>
2026-06-10 13:31:15 +0300

    Initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { createRoot } from "react-dom/client";
 import "modern-normalize/modern-normalize.css";
 import "./index.css";
 
-import App from "./App.tsx";
+import App from "./components/App/App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
